9fdd903fd29596bb3e59955906068c79eca50840
Quentin Burgniard <quentinburgniard@gmail.com>
2020-06-23 21:40:17 +0200

    feat(date): adding date

--- form-builder-1.0.0.js
+++ form-builder-1.0.0.js
@@ -1,16 +1,25 @@
 var formBuilder = formBuilder || {};
 
 formBuilder.version = '1.0.0';
-formBuilder.debug = formBuilder.debug || true;
+formBuilder.debug = formBuilder.debug || false;
 
-formBuilder.addField = function(fieldID, fieldType) {
+formBuilder.addField = function (fieldID, fieldType) {
   var div = document.createElement('div');
-  div.setAttribute('class', 'input-field col');
+  div.setAttribute('class', 'input-field');
   var input, label;
 
-  switch(fieldType) {
+  switch (fieldType) {
     case 'text':
       input = document.createElement('input');
+      input.setAttribute('id', fieldID);
+      input.setAttribute('type', 'text');
+      label = document.createElement('label');
+      label.setAttribute('for', fieldID);
+      label.innerText = fieldID;
+      break;
+    case 'date':
+      input = document.createElement('input');
+      input.setAttribute('class', fieldType);
       input.setAttribute('id', fieldID);
       input.setAttribute('type', 'text');
       label = document.createElement('label');
@@ -24,33 +33,35 @@ formBuilder.addField = function(fieldID, fieldType) {
   formBuilder.wrapper.appendChild(div);
 }
 
-formBuilder.init = function() {
+formBuilder.dynamicLoading = function () {
+  var prefix = 'https://raw.githubusercontent.com/quentinburgniard/form-builder/master/form-builder-';
+  var plugins = formBuilder.config.plugins || [];
+  if (document.querySelector('#form-builder-status')) plugins.push('status');
+  plugins.forEach(function(plugin) {
+    var script = document.createElement('script');
+    script.src = prefix + plugin + '-' + formBuilder.version + '.js';
+    document.head.appendChild(script);
+  });
+}
+
+formBuilder.init = function () {
+  formBuilder.log('Initialisation');
   formBuilder.wrapper = document.querySelector('#form-builder');
   var fields = formBuilder.config.fields || [];
   fields.forEach(field => formBuilder.addField(field.name, field.type));
 
-  if (document.querySelector('#form-builder-status')) {
-    var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/quentinburgniard/form-builder/master/form-builder-status-' + formBuilder.version + '.js';
-    document.head.appendChild(script);
-  }
+  formBuilder.dynamicLoading();
 }
 
-formBuilder.status = function() {
-  formBuilder.wrapper = document.querySelector(formBuilder.config.wrapper) || '';
-  var fields = formBuilder.config.fields || [];
-  fields.forEach(field => formBuilder.addField(field.name, field.type));
-}
-
-formBuilder.log = function(message) {
-  if(formBuilder.debug) {
+formBuilder.log = function (message) {
+  if (formBuilder.debug) {
     var prefix = 'Form Builder ' + formBuilder.version + ' ';
     console.log(prefix + message);
   }
 }
 
-formBuilder.saveFields = function() {
-  
+formBuilder.saveFields = function () {
+
 }
 
 formBuilder.init();
---
+++ form-builder-date-1.0.0.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.date');
+  M.Datepicker.init(elems);
+});
